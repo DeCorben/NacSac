055331241bcd7d3e92912a11c625101bb23546fa
DeCorben <DeCorben@AetheriaZ>
2018-11-09 13:12:23 -0500

    Begin Phase two
    Signed-off-by: DeCorben <DeCorben@AetheriaZ>

--- src/tucker/nacsac/RunNac.java
+++ src/tucker/nacsac/RunNac.java
@@ -3,6 +3,7 @@ package tucker.nacsac;
 public class RunNac extends RawTest
 {
 	private static Nac fact;
+	private static Stack stack;
 	
 	public static void main(String[] args)
 	{
@@ -15,12 +16,62 @@ public class RunNac extends RawTest
 		phaseOne();
 		phaseTwo();
 	}
+	
+	private void phaseFour() {
+		//Phase Four: links
+	}
+	
+	private void phaseThree() {
+		//Phase Three: Sac
+	}
 
 	private void phaseTwo() {
-		//
+		//Phase Two: Stack
+		//Stack.equals
+		stack =  new Stack(new Nac[] {new Nac("one","string","lorem"),
+				new Nac("two","number","3.1419"),
+				new Nac("ipsum","string","dolor")});
+		check(new Stack(new Nac[] {new Nac("one","string","lorem"),
+				new Nac("two","number","3.1419"),
+				new Nac("ipsum","string","dolor")}).equals(stack));
+		check(!new Stack().equals(stack));
+		test();
+		
+		//Stack.readNac
+		stack = new Stack(new Nac[] {new Nac("lorem","string","ipsum"),new Nac("dolor","string","sit"),new Nac("amet","string","consectetuer")});
+		check(new Nac("dolor","string","sit").equals(stack.readNac("dolor")));
+		check(new Nac("amet","string","consectetuer").equals(stack.readNac("amet")));
+		check(new Nac("lorem","string","ipsum").equals(stack.readNac("lorem")));
+		check(stack.readNac("decoy") == null);
+		test();
+		
+		//Stack.createNac
+		stack = new Stack();
+		stack.createNac(new Nac("lorem","string","ipsum"));
+		check(new Stack(new Nac[] {new Nac("lorem","string","ipsum")}).equals(stack));
+		stack.createNac(new Nac("lorem","number","3.1419"));
+		check(new Stack(new Nac[] {new Nac("lorem","string","ipsum")}).equals(stack));
+		test();
+		
+		//Stack.removeNac
+		stack = new Stack(new Nac[] {new Nac("lorem","string","ipsum"),new Nac("dolor","string","sit"),new Nac("amet","string","consectetuer")});
+		stack.deleteNac("dolor");
+		check(new Stack(new Nac[] {new Nac("lorem","string","ipsum"),new Nac("amet","string","consectetuer")}).equals(stack));
+		stack.deleteNac("amet");
+		check(new Stack(new Nac[] {new Nac("lorem","string","ipsum")}).equals(stack));
+		stack.deleteNac("lorem");
+		check(new Stack().equals(stack));
+		test();
+		
+		//Stack.updateNac
+		stack = new Stack(new Nac[] {new Nac("lorem","string","ipsum")});
+		stack.updateNac(new Nac("lorem","number","3.1419"));
+		check(new Stack(new Nac[] {new Nac("lorem","number","3.1419")}).equals(stack));
+		test();
 	}
 	
 	private void phaseOne() {
+		//Phase One: Nac
 		//Nac equals
 		fact = new Nac();
 		fact.setName("lorem");
@@ -41,5 +92,6 @@ public class RunNac extends RawTest
 	protected void resetTest() {
 		super.resetTest();
 		fact = null;
+		stack = null;
 	}
 }
---
+++ src/tucker/nacsac/Stack.java
@@ -0,0 +1,26 @@
+package tucker.nacsac;
+
+public class Stack {
+	public Stack() {
+		
+	}
+	
+	public Stack(Nac[] n) {
+		
+	}
+	
+	public void createNac(Nac n) {
+		
+	}
+	
+	public Nac readNac(String n) {
+		return null;
+	}
+	
+	public void updateNac(Nac n) {
+	}
+	
+	public void deleteNac(String n) {
+		
+	}
+}
